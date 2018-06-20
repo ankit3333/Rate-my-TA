b19f4e42ead87e7908573d9cf054f0795efe7ebe
hjames960 <hjames960@gmail.com>
2018-06-20 18:06:28 -0400

    sql injection prevention
    able to prevent sql injection without the use of prepared statement

--- form.php
+++ form.php
@@ -11,8 +11,7 @@ if (!$conn) {
 if(!mysqli_select_db($conn,'cse442_542_2018_summer_team04_db')){
   echo 'Database not selected';
 }
-$query1 = "SELECT  `coursenumber` FROM `coursecode`";
-$result = mysqli_query($conn,$query1);
+
        $course = mysqli_real_escape_string($conn,  $_POST['course']);
        $TAname =  mysqli_real_escape_string($conn, $_POST['TAname']);
        $description =  mysqli_real_escape_string($conn, $_POST['description']);
@@ -20,25 +19,18 @@ $result = mysqli_query($conn,$query1);
        $name =  mysqli_real_escape_string($conn, $_POST["name"]);
        $email= mysqli_real_escape_string($conn, $_POST["email"]);
        $pno= mysqli_real_escape_string($conn, $_POST["pno"]);
-       $experience= mysqli_real_escape_string($conn, $_POST["experience"]);
-       $query = "INSERT INTO TA_Rating(course,TAname,description,experience,comment,name,email,pno) VALUES (?, ?, ?, ?, ?, ? , ? , ? );";
-      $stmt= mysqli_stmt_init($conn);
-      if(!mysqli_stmt_prepare($stmt, $query)){
-        echo "SQL Error";
+       $experience=mysqli_real_escape_string($conn,$_POST["experience"]);
+       
+       $query = "INSERT INTO TA_Rating(course,TAname,description,experience,comment,name,email,pno) VALUES ('$course','$TAname','$description','$experience','$comment','$name','$email','$pno' )";
+      
+      if(!mysqli_query($conn,$query)){
+        
+        echo "Not inserted into databse";
+        
       }
       else{
-        mysqli_stmt_bind_parm($stmt, "ssssssss", $course, $TAname, $description,  $experience, $comment, $name, $email, $pno);
-        mysqli_stmt_execute($stmt);
-        echo "Inserted a new row in database" ;
+        echo "Inserted a new row in database";
       }
-      //if(!mysqli_query($conn,$query)){
-        
-       // echo "Not inserted into databse";
-        
-      //}
-      //else{
-       // echo "Inserted a new row in database";
-     // }
    
   header("refresh:2,url=Student_Panel.php");
 ?>
