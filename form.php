b97fcd2cb32e40f68bb3e41f980f2ab6f5685504
hjames960 <hjames960@gmail.com>
2018-06-20 17:32:00 -0400

    another update for preventing sql injection
    i fixed up the bug where the form can not be submitted after filling everything out

--- form.php
+++ form.php
@@ -1,55 +1,36 @@
 <?php
-
 $servername = "tethys.cse.buffalo.edu";
 $username = "ankitner";
 $password = "ChangeMe";
-
 // Create connection
-
 $conn = mysqli_connect($servername, $username, $password);
-
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 if(!mysqli_select_db($conn,'cse442_542_2018_summer_team04_db')){
   echo 'Database not selected';
 }
-
-
-
-       $course = $_POST['course'];
-
-       $TAname = $_POST['TAname'];
-
-       $description =  $_POST['description'];
-
-
-       $comment = $_POST["comment"];
-
-       $name =   $_POST["name"];
-
-       $email= $_POST["email"];
-
-       $pno= $_POST["pno"];
-
-       $experience= $_POST["experience"];
-
-
-       $stmt = $conn->prepare("INSERT INTO TA_Rating(course,TAname,description,experience,comment,name,email,pno) VALUES (?, ?, ?, ?, ?, ? , ? , ? )");
-
-    
-       $stmt->bind_param($stmt, "ssssssss", $course, $TAname, $description, $experience, $comment, $name, $email, $pno);
-       $stmt->execute();
+$query1 = "SELECT  `coursenumber` FROM `coursecode`";
+$result = mysqli_query($conn,$query1);
+       $course = mysqli_real_escape_string($conn,  $_POST['course']);
+       $TAname =  mysqli_real_escape_string($conn, $_POST['TAname']);
+       $description =  mysqli_real_escape_string($conn, $_POST['description']);
+       $comment =  mysqli_real_escape_string($conn, $_POST["comment"]);
+       $name =  mysqli_real_escape_string($conn, $_POST["name"]);
+       $email= mysqli_real_escape_string($conn, $_POST["email"]);
+       $pno= mysqli_real_escape_string($conn, $_POST["pno"]);
+       $experience= mysqli_real_escape_string($conn, $_POST["experience"]);
+       $query = "INSERT INTO TA_Rating(course,TAname,description,experience,comment,name,email,pno) VALUES (?, ?, ?, ?, ?, ? , ? , ? );";
+      $stmt= mysqli_stmt_init($conn);
+      if(!mysqli_stmt_prepare($stmt, $query)){
+        echo "SQL Error";
+      }
+      else{
+        mysqli_stmt_bind_parm($stmt, "ssssssss", $course, $TAname, $description,  $experience, $comment, $name, $email, $pno);
+        mysqli_stmt_execute($stmt);
         echo "Inserted a new row in database" ;
-        $stmt->close();
-        $conn->close();
-
-
-
-
-
+      }
       //if(!mysqli_query($conn,$query)){
         
        // echo "Not inserted into databse";
@@ -59,5 +40,5 @@ if(!mysqli_select_db($conn,'cse442_542_2018_summer_team04_db')){
        // echo "Inserted a new row in database";
      // }
    
-  header("url=sprint2.php");
+  header("refresh:2,url=Student_Panel.php");
 ?>
