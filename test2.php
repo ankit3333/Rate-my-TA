0a4db48e9208488a5aebd54a6a1354d57392b0f9
Minikhil <31946890+Minikhil@users.noreply.github.com>
2018-06-26 12:25:05 -0400

    Removed city menu

--- test2.php
+++ test2.php
@@ -20,25 +20,11 @@
                 });
             }else{
                 $('#state').html('<option value="">Select country first</option>');
-                $('#city').html('<option value="">Select state first</option>');
+
             }
         });
 
-        $('#state').on('change',function(){
-            var stateID = $(this).val();
-            if(stateID){
-                $.ajax({
-                    type:'POST',
-                    url:'ajaxData2.php',
-                    data:'state_id='+stateID,
-                    success:function(html){
-                        $('#city').html(html);
-                    }
-                });
-            }else{
-                $('#city').html('<option value="">Select state first</option>');
-            }
-        });
+
     });
     </script>
   </head>
@@ -70,9 +56,7 @@
         <option value="">Select country first</option>
     </select>
 
-    <select id="city">
-        <option value="">Select state first</option>
-    </select>
+
 
   </body>
 </html>
